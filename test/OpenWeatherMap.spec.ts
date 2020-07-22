c9c1beb032e0f81800041292f570415e796d24a7
YAMAMOTO Yuji <yuji-yamamoto@iij.ad.jp>
2020-07-22 13:23:42 +0900

    feat(OpenWeatherMap) Add `*ByCityId` APIs
    Ref: https://openweathermap.org/current#cityid
    
    NOTE: I've not successfully tested `dailyForecastByCityId` because I only have a free subscription

--- test/OpenWeatherMap.spec.ts
+++ test/OpenWeatherMap.spec.ts
@@ -160,7 +160,7 @@ describe('Testing OpenWeatherMap', () => {
     ).rejects.toThrow('http error')
   })
 
-    it('Should get the dailyForecastByCityName successfully', async () => {
+  it('Should get the dailyForecastByCityName successfully', async () => {
     const expectedUrl =
       'https://api.openweathermap.org/data/2.5/forecast/daily?APPID=xxx-xxx-xxx&q=Eindhoven%2Cnl&units=metric'
 
@@ -205,6 +205,133 @@ describe('Testing OpenWeatherMap', () => {
         countryCode: 'nl',
         name: 'Eindhoven'
       })
+    ).rejects.toThrow('http error')
+  })
+
+  it('Should get the weather byCityId successfully', async () => {
+    const expectedUrl =
+      'https://api.openweathermap.org/data/1.1/weather?APPID=xxx-xxx-xxx&id=12345&units=imperial'
+
+    const response: AxiosResponse = {
+      config: {},
+      data: sampleWeatherData,
+      headers: {},
+      status: 200,
+      statusText: 'success'
+    }
+    const stub = sandbox.stub(axios, 'get').resolves(response)
+
+    const options: IOpenWeatherMapApiOptions = {
+      apiVersion: '1.1',
+      key: 'xxx-xxx-xxx',
+      temperatureUnit: OpenWeatherMapApiUnits.Fahrenheit
+    }
+
+    const api = new OpenWeatherMapApi(options)
+
+    const data = await api.byCityId(12345)
+
+    sinon.assert.called(stub)
+    const call = stub.getCall(0)
+
+    expect(data).toBe(sampleWeatherData)
+    expect(call.args[0]).toBe(expectedUrl)
+  })
+
+  it('Should throw an error byCityId fails', async () => {
+    sandbox.stub(axios, 'get').rejects(new Error('http error'))
+
+    const options: IOpenWeatherMapApiOptions = {
+      key: 'xxx-xxx-xxx'
+    }
+
+    const api = new OpenWeatherMapApi(options)
+    await expect(
+      api.byCityId(23456)
+    ).rejects.toThrow('http error')
+  })
+
+  it('Should get the forecastByCityId successfully', async () => {
+    const expectedUrl =
+      'https://api.openweathermap.org/data/2.5/forecast?APPID=xxx-xxx-xxx&id=34567&units=metric'
+
+    const response: AxiosResponse = {
+      config: {},
+      data: sampleForecastData,
+      headers: {},
+      status: 200,
+      statusText: 'success'
+    }
+    const stub = sandbox.stub(axios, 'get').resolves(response)
+
+    const options: IOpenWeatherMapApiOptions = {
+      key: 'xxx-xxx-xxx'
+    }
+
+    const api = new OpenWeatherMapApi(options)
+
+    const data = await api.forecastByCityId(34567)
+
+    sinon.assert.called(stub)
+    const call = stub.getCall(0)
+
+    expect(data).toBe(sampleForecastData)
+    expect(call.args[0]).toBe(expectedUrl)
+  })
+
+  it('Should throw an error forecastByCityId fails', async () => {
+    sandbox.stub(axios, 'get').rejects(new Error('http error'))
+
+    const options: IOpenWeatherMapApiOptions = {
+      key: 'xxx-xxx-xxx'
+    }
+
+    const api = new OpenWeatherMapApi(options)
+
+    await expect(
+      api.forecastByCityId(45678)
+    ).rejects.toThrow('http error')
+  })
+
+  it('Should get the dailyForecastByCityId successfully', async () => {
+    const expectedUrl =
+      'https://api.openweathermap.org/data/2.5/forecast/daily?APPID=xxx-xxx-xxx&id=56789&units=metric'
+
+    const response: AxiosResponse = {
+      config: {},
+      data: sampleDailyForecastData,
+      headers: {},
+      status: 200,
+      statusText: 'success'
+    }
+    const stub = sandbox.stub(axios, 'get').resolves(response)
+
+    const options: IOpenWeatherMapApiOptions = {
+      key: 'xxx-xxx-xxx'
+    }
+
+    const api = new OpenWeatherMapApi(options)
+
+    const data = await api.dailyForecastByCityId(56789)
+
+    sinon.assert.called(stub)
+    const call = stub.getCall(0)
+
+    expect(data).toBe(sampleDailyForecastData)
+    expect(call.args[0]).toBe(expectedUrl)
+  })
+
+  it('Should throw an error dailyForecastByCityId fails', async () => {
+    sandbox.stub(axios, 'get').rejects(new Error('http error'))
+
+    const options: IOpenWeatherMapApiOptions = {
+      key: 'xxx-xxx-xxx'
+    }
+
+    const api = new OpenWeatherMapApi(options)
+
+    await expect(
+      api.dailyForecastByCityId(67890)
     ).rejects.toThrow('http error')
   })
 })
